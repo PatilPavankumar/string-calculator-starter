0987a26e5a2659ee0fec8a4c3062176586ab969e
PatilPavankumar <patilpavankumar6109@gmail.com>
2021-10-16 17:34:38 +0530

    Committing the code of calculator to work with two numbers with ',' as a delimiter. Also refactored the code.

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,11 +3,28 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-    	if(input.isEmpty())
+    	if(isEmpty(input))
         {
     		return 0;
         }
+    	if(input.length()==1)
+    	{
+    		return convertToInt(input);
+    	}
+    	else {
+    		String[] numbers=input.split(",");
+    		return calculateSum(numbers[0],numbers[1]);
+    	}
+    }
+    private boolean isEmpty(String input) {
+    	return input.isEmpty();
+    }
+    private int convertToInt(String input) {
     	return Integer.parseInt(input);
+    }
+    
+    private int calculateSum(String num1, String num2) {
+    	return Integer.parseInt(num1)+Integer.parseInt(num2);
     }
 
 }
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -4,17 +4,29 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
+
 class StringCalculatorShould {
 
-    @Test
+	private static StringCalculator stringCalculator;
+	
+	@BeforeAll
+	public static void init() {
+		stringCalculator= new StringCalculator();
+	}
+    
+	@Test
     void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
         assertEquals(0, stringCalculator.add(""));
     }
 
     @Test
     void string_with_single_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
+    }
+    
+    @Test
+    public void testTwoNumbersCommaDelimitted() {
+    	assertEquals(3, stringCalculator.add("1,2"));
     }
 }
